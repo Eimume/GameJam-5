1903f90e53657e10001df8d71e48a7b50a4f3b8f
Eimume <168836948+Eimume@users.noreply.github.com>
2024-10-18 22:41:50 +0700

    Add credit but Text still not fin

--- Assets/Script/MainMenuController.cs
+++ Assets/Script/MainMenuController.cs
@@ -1,9 +1,21 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+
+    public GameObject creditUI;
+    public GameObject exitButton;
+    public Text TextNames;
     
+    private void Start()
+    {
+        creditUI.SetActive(false);
+        exitButton.SetActive(false);
+        TextNames.gameObject.SetActive(false);
+    }
     public void StartGame()
     {
         SceneManager.LoadScene("Map"); // Load the Map scene when the player clicks start
@@ -14,5 +26,19 @@ public class MainMenuController : MonoBehaviour
     {
         Application.Quit(); // Quit the application
         Debug.Log("Game is exiting...");
+    }
+
+    public void Showcredit()
+    {
+        creditUI.SetActive(true);
+        exitButton.SetActive(true);
+        TextNames.gameObject.SetActive(true);
+    }
+
+    public void Closecredit()
+    {
+        creditUI.SetActive(false);
+        exitButton.SetActive(false);
+        TextNames.gameObject.SetActive(false);
     }
 }
